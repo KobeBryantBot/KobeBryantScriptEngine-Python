4dc0f0d0f2d5d502f1173334deced18dcc613d0c
Fluorine <186297309+DivanadiumPentaoxide@users.noreply.github.com>
2024-11-07 20:16:23 +0800

    add api test

--- src/api/Logger.cpp
+++ src/api/Logger.cpp
@@ -1,3 +1,4 @@
+#include "api/Logger.hpp"
 #include <pybind11/chrono.h>
 #include <pybind11/complex.h>
 #include <pybind11/embed.h>
@@ -5,3 +6,38 @@
 #include <pybind11/stl.h>
 
 namespace py = pybind11;
+
+class ScriptLogger : public Logger {
+public:
+    ScriptLogger() : Logger() {}
+    ScriptLogger(std::string const& title) : Logger(title) {}
+
+    void fatal(std::string const& content) { return printStr(LogLevel::Fatal, content); }
+    void error(std::string const& content) { return printStr(LogLevel::Error, content); }
+    void warn(std::string const& content) { return printStr(LogLevel::Warn, content); }
+    void info(std::string const& content) { return printStr(LogLevel::Info, content); }
+    void debug(std::string const& content) { return printStr(LogLevel::Debug, content); }
+};
+
+
+PYBIND11_EMBEDDED_MODULE(LoggerAPI, m) {
+    py::class_<ScriptLogger> LoggerClass(m, "Logger");
+    LoggerClass.def(py::init<>())
+        .def(py::init<std::string const&>())
+        .def("setTitle", &ScriptLogger::setTitle)
+        .def("setLevel", &ScriptLogger::setLevel)
+        .def("setFile", &ScriptLogger::setFile)
+        .def("fatal", &ScriptLogger::fatal)
+        .def("error", &ScriptLogger::error)
+        .def("warn", &ScriptLogger::warn)
+        .def("info", &ScriptLogger::info)
+        .def("debug", &ScriptLogger::debug);
+
+    py::enum_<Logger::LogLevel>(LoggerClass, "LogLevel")
+        .value("Fatal", Logger::LogLevel::Fatal)
+        .value("Error", Logger::LogLevel::Error)
+        .value("Warn", Logger::LogLevel::Warn)
+        .value("Info", Logger::LogLevel::Info)
+        .value("Debug", Logger::LogLevel::Debug)
+        .export_values();
+}
--- src/api/PacketSender.cpp
+++ src/api/PacketSender.cpp
@@ -1,3 +1,4 @@
+#include "api/PacketSender.hpp"
 #include <pybind11/chrono.h>
 #include <pybind11/complex.h>
 #include <pybind11/embed.h>
@@ -6,43 +7,12 @@
 
 namespace py = pybind11;
 
-class MyClass {
-public:
-    void myMethod() { py::print("Called myMethod from MyClass"); }
-};
-
-class AnotherClass {
-public:
-    int anotherMethod(int value) {
-        py::print("Called anotherMethod from AnotherClass");
-        return value * 2; // 示例返回值
-    }
-};
-
-#include "api/Logger.hpp"
-#include "api/Schedule.hpp"
-
-void stdFunction(std::function<void()> func) {
-    // 调用传入的函数
-    Logger().warn("Call");
-    Schedule::addRepeatTask(
-        std::chrono::seconds(5),
-        [=] {
-            Logger().warn("try call {}", func ? true : false);
-            func();
-        },
-        true
-    );
-}
-
-PYBIND11_EMBEDDED_MODULE(my_module, m) {
-    // 绑定 MyClass 类
-    py::class_<MyClass>(m, "MyClass").def(py::init<>()).def("myMethod", &MyClass::myMethod);
-
-    // 绑定 AnotherClass 类
-    py::class_<AnotherClass>(m, "AnotherClass").def(py::init<>()).def("anotherMethod", &AnotherClass::anotherMethod);
-
-    // 导出一个独立的函数
-    m.def("myFunction", []() { py::print("Called myFunction"); });
-    m.def("stdFunction", &stdFunction);
+PYBIND11_EMBEDDED_MODULE(PacketSenderAPI, m) {
+    py::class_<PacketSender>(m, "PacketSender")
+        .def_static(
+            "getInstance",
+            [] { return std::unique_ptr<PacketSender, py::nodelete>(&PacketSender::getInstance()); }
+        )
+        .def("sendRawPacket", py::overload_cast<std::string const&>(&PacketSender::sendRawPacket))
+        .def("sendGroupMessage", py::overload_cast<uint64_t, std::string const&>(&PacketSender::sendGroupMessage));
 }
