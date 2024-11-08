829bb214d22870336f232cb94309518d1418df7d
Fluorine <186297309+DivanadiumPentaoxide@users.noreply.github.com>
2024-11-08 22:06:37 +0800

    add CommandRegistry

--- src/api/CommandRegistry.cpp
+++ src/api/CommandRegistry.cpp
@@ -1,3 +1,4 @@
+#include <api/CommandRegistry.hpp>
 #include <pybind11/chrono.h>
 #include <pybind11/complex.h>
 #include <pybind11/embed.h>
@@ -5,3 +6,19 @@
 #include <pybind11/stl.h>
 
 namespace py = pybind11;
+
+PYBIND11_EMBEDDED_MODULE(CommandRegistryAPI, m) {
+    py::class_<CommandRegistry>(m, "CommandRegistry")
+        .def_static(
+            "getInstance",
+            [] { return std::unique_ptr<CommandRegistry, py::nodelete>(&CommandRegistry::getInstance()); }
+        )
+        .def(
+            "registerCommand",
+            py::overload_cast<std::string const&, std::function<void(std::vector<std::string> const&)>>(
+                &CommandRegistry::registerCommand
+            )
+        )
+        .def("unregisterCommand", py::overload_cast<std::string const&>(&CommandRegistry::unregisterCommand))
+        .def("executeCommand", &CommandRegistry::executeCommand);
+}
